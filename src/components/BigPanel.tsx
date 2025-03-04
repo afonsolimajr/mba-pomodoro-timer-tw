6c73f53f121cfea2cf7a78132501723c51c115a8
Afonso Lima <afonsolima@gmail.com>
2025-03-04 17:20:59 -0300

    home layout and responsivity

--- src/components/BigPanel.tsx
+++ src/components/BigPanel.tsx
@@ -2,15 +2,19 @@ import BigNumber from "./BigNumber";
 
 export default function BigPanel() {
   return (
-    <div className="flex gap-5 items-center justify-center">
-      <BigNumber />
-      <BigNumber />
-      <div className="flex flex-col gap-6 items-center justify-center h-full">
+    <div className="flex flex-col sm:flex-row gap-5 items-center justify-center">
+      <div className="flex gap-5 items-center justify-center">
+        <BigNumber />
+        <BigNumber />
+      </div>
+      <div className="hidden sm:flex flex-col gap-6 items-center justify-center h-full">
         <div className="flex h-5 w-5 rounded-full bg-[var(--green)]" />
         <div className="flex h-5 w-5 rounded-full bg-[var(--green)]" />
       </div>
-      <BigNumber />
-      <BigNumber />
+      <div className="flex gap-5 items-center justify-center">
+        <BigNumber />
+        <BigNumber />
+      </div>
     </div>
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,11 +3,19 @@ import BigPanel from "../components/BigPanel";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full items-center gap-3">
-      <div>form</div>
+    <div className="flex flex-col w-full h-full items-center justify-center gap-10 md:gap-20">
+      <div className="flex w-full max-w-2xl flex-col md:flex-row  items-center justify-between gap-2">
+        <span>Vou trabalhar em</span>
+        <input className="" placeholder="Dê um nome para o seu projeto" />
+        <div className="flex items-center justify-between gap-2">
+          <span>durante</span>
+          <input className="w-12 text-center border-b" placeholder="-00+" />
+          <span>minutos.</span>
+        </div>
+      </div>
       <BigPanel />
-      <div>
-        <button className="flex gap-3 w-2xl bg-green-800 items-center justify-center">
+      <div className="flex w-full px-3 justify-center items-center">
+        <button className="flex gap-3 w-full max-w-2xl bg-green-800 items-center justify-center">
           <Play size={28} /> Começar
         </button>
       </div>
